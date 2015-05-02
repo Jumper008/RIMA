fbc604efb9b8f46bab927fa8f102b141b8063a50
Alejandro Zamudio Guajardo <alex.zamudio88@gmail.com>
2015-05-02 16:47:35 -0500

    Almost final controller commit

--- src/com/rima/controls/mostrarListaArticulosPublicadosAutor.java
+++ src/com/rima/controls/mostrarListaArticulosPublicadosAutor.java
@@ -4,3 +4,22 @@
  *Version: 1.0.0
  *
  */
+ package com.rima.controls;
+ 
+ import java.util.*;
+ import java.io.*;
+ import com.rima.entities.*;
+ 
+ public class mostrarListaArticulosPublicadosAutor {
+	 Articulo arArticulo;
+	 private transient Conexion conConexion;
+	 
+	 public mostrarListaArticulosPublicadosAutor() {
+		 conConexion = new Conexion();
+		 arArticulo = new Articulo(conConexion);
+	 }
+	 
+	 public Vector<Articulo> obtenerListaArticulosPublicadosAutor(int iIDPersona) {
+		 return arArticulo.consultarArticulosAutor(iIDPersona);
+	 }
+ }
--- src/com/rima/controls/mostrarListaAutores.java
+++ src/com/rima/controls/mostrarListaAutores.java
@@ -4,3 +4,22 @@
  *Version: 1.0.0
  *
  */
+ package com.rima.controls;
+ 
+ import java.util.*;
+ import java.io.*;
+ import com.rima.entities.*;
+ 
+ public class mostrarListaAutores {
+	 Autor auAutor;
+	 private transient Conexion conConexion;
+	 
+	 public mostrarListaAutores() {
+		 conConexion = new Conexion();
+		 auAutor = new Autor(conConexion);
+	 }
+	 
+	 public Vector<Autor> obtenerListaAutores() {
+		 return auAutor.mostrarNombreDeAutores();
+	 }
+ }
