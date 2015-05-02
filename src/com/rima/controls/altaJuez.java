65dc51b460e2dea648d765f8713d6b6fc9c92d9a
Mildred Gatica <mildred_gatica@hotmail.com>
2015-05-01 20:05:57 -0500

    altaJuez

--- src/com/rima/controls/altaJuez.java
+++ src/com/rima/controls/altaJuez.java
@@ -4,3 +4,37 @@
  *Version: 1.0.0
  *
  */
+ package com.rima.controls;
+ 
+ import java.util.*;
+ import java.sql.*;
+ import java.io.*;
+ import java.util.Date;
+ import com.rima.entities.*;
+ 
+ public class altaJuez {
+	 Juez juJuez;
+	 private transient Conexion conConexion;
+	 protected Statement stmt;
+	 
+	 public altaJuez() {
+		 conConexion = new Conexion();
+		 juJuez = new Juez(conConexion);
+	 }
+	 
+	 public boolean agregarJuez(String sNombre, String sCorreo, String sContrasena, 
+	 		Date dFechaNacimiento, Date dFechaIngreso, Date dFechaVencimiento, boolean bActivo) {
+	 	
+			 int iIDPersona = juJuez.generarID();
+			 juJuez.setiIDPersona(iIDPersona);
+			 juJuez.setsNombre(sNombre);
+			 juJuez.setsCorreo(sCorreo);
+			 juJuez.setsContrasena(sContrasena);
+			 juJuez.setdFechaNacimiento(dFechaNacimiento);
+			 juJuez.setdFechaIngreso(dFechaIngreso);
+			 juJuez.setdFechaVencimiento(dFechaVencimiento);
+			 juJuez.setbActivo(bActivo);
+			 juJuez.agregarJuez(juJuez);
+		 return false;
+	 }
+ }
